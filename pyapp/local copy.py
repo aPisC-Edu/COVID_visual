0f8066578fed0257d027a5fa75041f065d4c9a55
Bendegúz Cseresnyés <cser.bende@gmail.com>
2020-05-07 21:33:37 +0200

    fixed gauss interpolation

--- pyapp/local copy.py
+++ pyapp/local copy.py
@@ -47,12 +47,22 @@ y3 = -y2
 # Fit your model
 p1 = np.polyfit(x, y, 1) # for linear fitting
 p2 = np.polyfit(x, y2, 1) # for exponential fitting
-p3 = np.polyfit(x, y3, 2) # for gauss fitting
-
+p3 = np.polyfit(x, y2, 2)
 
 # Visualize the forecasts (blue=train, green=forecasts)
-plt.plot(x, y, c='green')
-plt.plot(x, p1[0] * x + p1[1], c='blue') # linear
-plt.plot(x, math.exp(p2[1]) * np.exp(x * p2[0]) , c='blue') # exponential
-plt.plot(x, math.exp(p3[2]) * np.exp(-x^2 * p3[0] - x*p3[1]), c='blue') # gauss
+#plt.plot(x, y2)
+#plt.plot(x, np.polyval(p3, x))
+#plt.show()
+
+
+plt.plot(x, y)
+x = np.arange(100) - 20
+plt.plot(x, p1[0] * x + p1[1]) # linear
+#plt.plot(x, math.exp(p2[1]) * np.exp(x * p2[0])) # exponential
+
+gc = p3[0]
+gb = -p3[1]/ (2*p3[0])
+ga = math.exp(p3[2] - p3[1] * p3[1] / (4 * p3[0]) )
+
+plt.plot(x, ga * np.exp(np.power(x - gb, 2) * gc )) # gauss
 plt.show()
